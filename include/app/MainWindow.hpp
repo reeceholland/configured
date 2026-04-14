defd250023692160588717a9216a7fb0acc33dbc
reece-the-robot <17493785+reece-the-robot@users.noreply.github.com>
2026-04-14 18:24:46 +0800

    json exporter added

--- include/app/MainWindow.hpp
+++ include/app/MainWindow.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+
 #include "core/GitService.hpp"
 
 class QStackedWidget;
@@ -12,14 +13,13 @@ class HomeScreenWidget;
 class EditorScreenWidget;
 class HelpScreenWidget;
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
   Q_OBJECT
 
-public:
+ public:
   MainWindow();
 
-private:
+ private:
   void showHome();
   void showEditor();
   void showHelp();
@@ -28,32 +28,35 @@ private:
   void promptAndCreateProject();
   void updateGitUiVisibility();
   QString currentProjectWorkingDirectory() const;
+  void exportParametersToJson();
 
-private:
-  QStackedWidget *stack_ = nullptr;
+ private:
+  QStackedWidget* stack_ = nullptr;
 
-  HomeScreenWidget *home_ = nullptr;
-  EditorScreenWidget *editor_ = nullptr;
-  HelpScreenWidget *help_ = nullptr;
+  HomeScreenWidget* home_ = nullptr;
+  EditorScreenWidget* editor_ = nullptr;
+  HelpScreenWidget* help_ = nullptr;
 
-  QToolBar *toolbar_ = nullptr;
-  QToolButton *gitButton_ = nullptr;
+  QToolBar* toolbar_ = nullptr;
+  QToolButton* gitButton_ = nullptr;
 
-  QAction *saveProjectAction_ = nullptr;
-  QAction *addChildAction_ = nullptr;
-  QAction *removeItemAction_ = nullptr;
-  QAction *goHomeAction_ = nullptr;
-  QAction *projectMetadataAction_ = nullptr;
-  QAction *versionAction_ = nullptr;
-  QAction *helpAction_ = nullptr;
-  QAction *gitButtonAction_ = nullptr;
+  QAction* saveProjectAction_ = nullptr;
+  QAction* addChildAction_ = nullptr;
+  QAction* removeItemAction_ = nullptr;
+  QAction* goHomeAction_ = nullptr;
+  QAction* projectMetadataAction_ = nullptr;
+  QAction* versionAction_ = nullptr;
+  QAction* helpAction_ = nullptr;
+  QAction* gitButtonAction_ = nullptr;
+  QAction* exportXmlAction_ = nullptr;
+  QAction* exportJsonAction_ = nullptr;
 
-  QAction *gitInitAction_ = nullptr;
-  QAction *gitStatusAction_ = nullptr;
-  QAction *gitCommitAction_ = nullptr;
-  QAction *gitConfigAction_ = nullptr;
+  QAction* gitInitAction_ = nullptr;
+  QAction* gitStatusAction_ = nullptr;
+  QAction* gitCommitAction_ = nullptr;
+  QAction* gitConfigAction_ = nullptr;
 
-  QAction *exportAction_ = nullptr;
+  QAction* exportAction_ = nullptr;
 
   GitService gitService_;
 };
