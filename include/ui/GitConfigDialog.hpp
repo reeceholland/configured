2f9c788e19befd320b7edbe5f5485ce97f435309
reece-the-robot <17493785+reece-the-robot@users.noreply.github.com>
2026-05-30 03:59:04 +0800

    clang-format

--- include/ui/GitConfigDialog.hpp
+++ include/ui/GitConfigDialog.hpp
@@ -6,25 +6,23 @@ class QLineEdit;
 class QRadioButton;
 class GitService;
 
-class GitConfigDialog : public QDialog
-{
-    Q_OBJECT
+class GitConfigDialog : public QDialog {
+  Q_OBJECT
 
-public:
-    explicit GitConfigDialog(GitService *gitService,
-                             const QString &workingDir,
-                             QWidget *parent = nullptr);
+ public:
+  explicit GitConfigDialog(GitService* gitService, const QString& workingDir,
+                           QWidget* parent = nullptr);
 
-private:
-    void loadCurrentValues();
-    void saveValues();
+ private:
+  void loadCurrentValues();
+  void saveValues();
 
-private:
-    GitService *gitService_ = nullptr;
-    QString workingDir_;
+ private:
+  GitService* gitService_ = nullptr;
+  QString workingDir_;
 
-    QLineEdit *nameEdit_ = nullptr;
-    QLineEdit *emailEdit_ = nullptr;
-    QRadioButton *globalRadio_ = nullptr;
-    QRadioButton *localRadio_ = nullptr;
+  QLineEdit* nameEdit_ = nullptr;
+  QLineEdit* emailEdit_ = nullptr;
+  QRadioButton* globalRadio_ = nullptr;
+  QRadioButton* localRadio_ = nullptr;
 };
